e6b4d3b2cf849a9d5631166ed198626524050d7e
hoangtran0935 <21110031@student.hcmute.edu.vn>
2023-10-22 15:07:06 +0700

    update authentication

---
+++ src/main/java/applications/authentication/application/SignInApplication.java
@@ -0,0 +1,30 @@
+package applications.authentication.application;
+
+import applications.account.Account;
+import applications.account.service.AccountService;
+import applications.user.User;
+import applications.user.service.UserService;
+import org.apache.commons.lang3.StringUtils;
+
+public class SignInApplication {
+    UserService userService = new UserService();
+    AccountService accountService = new AccountService();
+
+    public User signIn(String username, String password) throws Exception {
+        if (StringUtils.isEmpty(username)) {
+            throw new Exception("Username can not be not empty");
+        }
+        if (StringUtils.isEmpty(password)) {
+            throw new Exception("Password can not be not empty");
+        }
+        Account account = accountService.getUserByAccount(username);
+        if (account == null || StringUtils.isEmpty(account.getUsername())) {
+            throw new Exception("Account does not exist");
+        }
+
+        if (!account.getPassword().equals(password)) {
+            throw new Exception("Password does not right");
+        }
+        return userService.getUserById(account.getUser_id());
+    }
+}
---
+++ src/main/java/applications/authentication/controller/SignInServletController.java
@@ -0,0 +1,55 @@
+package applications.authentication.controller;
+
+
+import applications.authentication.application.SignInApplication;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "SignInServletController", urlPatterns = {"/sign_in"})
+public class SignInServletController extends HttpServlet {
+    SignInApplication signInApplication = new SignInApplication();
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String url = "/sign_in/sign_in.html";
+
+        String action = request.getParameter("action");
+        if (action == null) {
+            action = "submit";  // default action
+        }
+
+        if (action.equals("submit")) {
+            url = "/sign_in/sign_in.html";    // the "join" page
+        }
+        if (action.equals("add")) {
+            String userName = request.getParameter("username");
+            String password = request.getParameter("password");
+            try {
+                signInApplication.signIn(userName, password);
+            } catch (Exception e) {
+                url = "/sign_in/error_notification.jsp";
+                request.setAttribute("error",e.getMessage());
+
+                getServletContext()
+                        .getRequestDispatcher(url)
+                        .forward(request, response);
+            }
+
+            url = "/user/user.jsp";
+        }
+        getServletContext()
+                .getRequestDispatcher(url)
+                .forward(request, response);
+    }
+    @Override
+    protected void doGet(HttpServletRequest request,
+                         HttpServletResponse response)
+            throws ServletException, IOException {
+
+        doPost(request, response);
+    }
+}
